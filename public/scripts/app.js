531186096845882d8ceac9310922a5a7b99921e5
99hsueh <99hhsueh@gmail.com>
2017-03-03 04:18:02 +0000

    changed files to ejs and started working on client side codes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
+  //element for placed order to order list
   function createOrderElement(invoice){
 
-    const $order = $('.containCustOrders').addClass('.custOrders')
-    const orderInfo = '<div class="row orderInfo">'
-    const $orderId = $(".col-md-4 orderId").text(invoice.orders.id);
-    const $custName = $(".col-md-4 custName").text(invoice.users.username);
-    const $phoneNumber = $(".col-md-4 phoneNumber").text(invoice.users.phone_number);
-    const foodItem = '<div class="row foodItem">'
-    const space2col = '<div class =".col-md-2"></div>';
-    const $itemName = $(".col-md-6 itemName").text(invoice.items.name);
-    const $quantity = $(".col-md2 quantity").text(invoice.items.id[?]);
-    const responseToOrder = '<div class="row responseToOrder">'
-    const informMsg = '<div class="text-center col-md-4">Please inform customer of order pick up time</div>'
-    const timeInput = '<input type="text" class="form-control" id="deliveryTime" placeholder="minutes">'
-    const submitTime = '<a href="#" class="btn btn-primary btn-lg">submit</a>'
+    var $order = $('.containCustOrders').addClass('.custOrders')
+    var orderInfo = '<div class="row orderInfo">'
+    var $orderId = $(".col-md-4 orderId").text(invoice.orders.id);
+    var $custName = $(".col-md-4 custName").text(invoice.users.username);
+    var $phoneNumber = $(".col-md-4 phoneNumber").text(invoice.users.phone_number);
+    var foodItem = '<div class="row foodItem">'
+    var space2col = '<div class =".col-md-2"></div>';
+    var $itemName = $(".col-md-6 itemName").text(invoice.items.name);
+    var $quantity = $(".col-md2 quantity").text(invoice.items.id[?]);
+    var responseToOrder = '<div class="row responseToOrder">'
+    var informMsg = '<div class="text-center col-md-4">Please inform customer of order pick up time</div>'
+    var timeInput = '<input type="text" class="form-control" id="deliveryTime" placeholder="minutes">'
+    var submitTime = '<a href="#" class="btn btn-primary btn-lg">submit</a>'
 
     $order.append(orderInfo, foodItem, responseToOrder);
     orderInfo.append($orderId, $custName, $phoneNumber);
@@ -23,23 +24,54 @@ $(document).ready(function(){
     return $order;
   }
 
+  //element for order to queue list
+  function createQueueElement(invoice){
+
+    var $order = $('.containPendingOrders').addClass('.custQueue')
+    var orderInfo = '<div class="row orderInfo">'
+    var $orderId = $(".col-md-4 orderId").text(invoice.orders.id);
+    var $custName = $(".col-md-4 custName").text(invoice.users.username);
+    var $phoneNumber = $(".col-md-4 phoneNumber").text(invoice.users.phone_number);
+    var foodItem = '<div class="row foodItem">'
+    var space2col = '<div class =".col-md-2"></div>';
+    var $itemName = $(".col-md-6 itemName").text(invoice.items.name);
+    var $quantity = $(".col-md2 quantity").text(invoice.items.id[?]);
+    var clearQueue = '<div class="row clearQueue">';
+    var statusOfOrder = '<div class="text-center col-md-4">Pending for pickup....</div>';
+    var completeOrder = '<div class="text-center col-md-4"><a href="#" class="btn btn-primary btn-lg">Complete Order</a></div>';
+
+
+    $order.append(orderInfo, foodItem, clearQueue);
+    orderInfo.append($orderId, $custName, $phoneNumber);
+    foodItem.append(space2col, $itemName, $quantity, space2col);
+    clearQueue.append(space2col, statusOfOrder, completeOrder, space2col);
+
+    return $order;
+  }
+
   function renderOrders(orders) {
-    // loops through tweets
+    var orders = {
+
+    }
     for(id of orders){
-      // calls createTweetElement for each tweet
-      // takes return value and appends it to the tweets container
       $('.custOrders').append(createOrderElement(id));
     }
   }
 
-  function loadOrders() {
-    $.ajax({
-        url: 'restaurants/:restaurants_id/orders',
-        method: 'GET',
-        success: renderOrders
-    });
-  }
-  //RUN THIS FUNCTION EVERY 15 SEC WHEN OWNER LOGGED IN
-  loadOrders();
+  // function loadOrders() {
+  //   $.ajax({
+  //       url: 'restaurants/id1/orders',
+  //       method: 'GET',
+  //       success: renderOrders
+  //   });
+  // }
+  // //RUN THIS FUNCTION EVERY 15 SEC WHEN OWNER LOGGED IN
+  // loadOrders();
+
+  renderOrders();
+
+
+
+
 
 });
