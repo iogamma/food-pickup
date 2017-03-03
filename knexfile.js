25020844f75402d4da8a3309edefb04e7e80e53f
Hal Tang <hal.wh.tang@gmail.com>
2017-03-03 20:16:56 +0000

    Restructured file directories and assigned  environment variables

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,23 @@
-// Update with your config settings.
+require('dotenv').config();
+
 module.exports = {
 
   development: {
-    client: 'pg',
+    client     : process.env.DB_CLIENT,
     connection: {
-      database: 'midterm',
-      user:     'labber',
-      password: 'labber'
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      port     : process.env.DB_PORT,
+      ssl      : process.env.DB_SSL
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
-
 };
---
+++ server.js
@@ -0,0 +1,56 @@
+"use strict";
+
+require('dotenv').config();
+
+const PORT        = process.env.PORT || 8080;
+const ENV         = process.env.ENV || "development";
+const logger      = require('winston');
+logger.level      = process.env.LOG_LEVEL || 'info';
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const cookieSess  = require('cookie-session');
+const sass        = require("node-sass-middleware");
+const app         = express();
+
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const morgan      = require('morgan');
+const knexLogger  = require('knex-logger');
+
+const db = require("./lib/db.js");
+
+// Seperated Routes for each Resource
+const queries = require("./lib/queries.js")(db);
+const Routes = require("./routes/items")(queries);
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
+// Mount all resource routes
+app.use("/", Routes);
+
+// Home page
+app.get("/", (req, res) => {
+  res.render("menu_orders");
+});
+
+const server = app.listen(PORT, () => {
+  const address = server.address();
+  logger.info('Server listening on port:' , address.port);
+});
+
