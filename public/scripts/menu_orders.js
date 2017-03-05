c77aea5fcc48787a81e958e5949973135e4c2a80
Hal Tang <hal.wh.tang@gmail.com>
2017-03-05 00:10:54 +0000

    Added event handlers for menu orders script

--- public/scripts/menu_orders.js
+++ public/scripts/menu_orders.js
@@ -7,23 +7,68 @@ $(document).ready(function(){
 
     //TODO: Ajax POST to server for that item with the input value
     $.ajax({
-       method: 'POST',
-       url: '/cart/itemId'
-    }).then( funcation() {
-
+      method: 'POST',
+      url: '/cart/' + itemId
+    }).then( function() {
+      // Optional HTML confirmation to user
     }).fail( function() {
-
+      alert("Sorry, we were unable to change your item quantity! Please contact support.");
     });
   });
 
   $('.plus_item').on('click', function(event) {
     event.preventDefault();
-    //TODO: AJAX POST to server to increment quantity for that item.
-
+    //TODO: grab the quantity inside the data attribute
+    var currentQty = $(this).parent("div")
+                            .siblings(".qty_value")
+                            .find(".qty_input")
+                            .data("qty");
+    //TODO: AJAX POST to server the value of the data attribute
+    $.ajax({
+      method: 'POST',
+      url: '/cart/' + (currentQty + 1).toString();
+    }).then( function() {
+      // add 1 to quantity inside data attribute
+      currentQty += 1;
+      // update new quantity for value
+      $(this).parent("div")
+             .siblings(".qty_value")
+             .find(".qty_input")
+             .attr("value", currentQty);
+      $(this).parent("div")
+             .siblings(".qty_value")
+             .find(".qty_input")
+             .data("qty", currentQty);
+    }).fail( function() {
+      alert("Sorry, were unable to add 1 to your quantity! Please contact suppport.");
+    });
   });
 
   $('.minus_item').on('click', function(event) {
     event.preventDefault();
+    //TODO: grab the quantity inside the data attribute
+    var currentQty = $(this).parent("div")
+                            .siblings(".qty_value")
+                            .find(".qty_input")
+                            .data("qty");
     //TODO: AJAX POST to server to decrement quantity for that item.
+        $.ajax({
+      method: 'POST',
+      url: '/cart/' + (currentQty - 1).toString();
+    }).then( function() {
+      // add 1 to quantity inside data attribute
+      currentQty += 1;
+      // update new quantity for value
+      $(this).parent("div")
+             .siblings(".qty_value")
+             .find(".qty_input")
+             .attr("value", currentQty);
+      $(this).parent("div")
+             .siblings(".qty_value")
+             .find(".qty_input")
+             .data("qty", currentQty);
+    }).fail( function() {
+      alert("Sorry, were unable to add 1 to your quantity! Please contact suppport.");
+    });
   });
 });
